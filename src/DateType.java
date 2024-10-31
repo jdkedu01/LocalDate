e24132e33c5cc00705d75993ba49040e34972564
guman <jdk@deu.ac.kr>
2024-10-31 16:09:07 +0900

    Date to LocalDate.V1

--- src/DateType.java
+++ src/DateType.java
@@ -1,8 +1,4 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -30,26 +26,31 @@ public class DateType {
 		} catch(SQLException e) {
 			System.out.println("시퀀스삭제오류 : "+e.getMessage());
 		}
-		// Oracle : create or replace table 없음.
-		// Oracle : create table if not exists 없음.
 		query = "create sequence idSEQ";
 		try {
 			stmt.executeUpdate(query);
 		} catch(SQLException e) {
 			System.out.println("시퀀스생성오류 : "+e.getMessage());
 		}
-		query = "create table 임시주문 (" +
-				"주문번호 NUMBER(10)," +
-				"주문자 VARCHAR2(20)," +
-				"구매상품 VARCHAR2(20)," +
-				"기록시작일 DATE," +     //오라클은 Time type이 없음
-				"주문일시 TIMESTAMP )";
 		try {
-			stmt.executeUpdate(query);
-			stmt.close();
-		} catch(SQLException e) {
-			System.out.println("테이블생성오류 : "+e.getMessage());
+			DatabaseMetaData dbMetaData = con.getMetaData();
+			String tableName = "임시주문";
+			ResultSet tables = dbMetaData.getTables(null, null, tableName.toUpperCase(), new String[] {"TABLE"});
+				if (tables.next()) {		System.out.println("Table " + tableName + " exists."); }
+				else {
+					query = "create table 임시주문 (" +
+							"주문번호 NUMBER(10)," +
+							"주문자 VARCHAR2(20)," +
+							"구매상품 VARCHAR2(20)," +
+							"기록시작일 DATE," +     //오라클은 Time type이 없음
+							"주문일시 TIMESTAMP )";
+					stmt.executeUpdate(query);
+					stmt.close();
+				}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
 	}
 	public void insert(String 주문자,  String 주문상품) {
 		long milli = System.currentTimeMillis();
@@ -75,7 +76,8 @@ public class DateType {
 	}
 	public void select() {
 		String query = "SELECT * from 임시주문";
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		//DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH");
 		try {
 			stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
@@ -85,10 +87,15 @@ public class DateType {
 				System.out.print("\t" + rs.getString(3) );
 				System.out.print("\t" + rs.getDate(4).toLocalDate() );
 				//System.out.print("\t" + rs.getTime(5).toLocalDateTime().format(formatter) );
-				System.out.print("\t" + rs.getTimestamp(5).toLocalDateTime().format(formatter) + "\n" );
-				System.out.print("\t요일 : " + rs.getTimestamp(5).toLocalDateTime().getDayOfWeek());  //월요일이 1
-				int year = rs.getDate(4).toLocalDate().getYear();
-				System.out.print("\t시작년도 : " + year + "\n");
+				System.out.print("\t" + rs.getTimestamp(5).toLocalDateTime().format(formatter) );
+				System.out.println("\t요일 : " + rs.getTimestamp(5).toLocalDateTime().getDayOfWeek());
+
+				LocalDate 오늘 = LocalDate.now();
+				System.out.print("\t\t" + 오늘);
+				System.out.print("\t년도 : 윤년여부 ==> " + 오늘.getYear() + " : "  + 오늘.isLeapYear());
+
+				LocalDate 백일뒤 = 오늘.plusDays(100);
+				System.out.println("\t백일뒤 : " + 백일뒤+ "\n");
 			}
 			stmt.close();
 		} catch(SQLException e) {
